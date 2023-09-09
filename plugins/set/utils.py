e07f7f9a39074e4810b6c17cf56983e41b647188
MarshSandpiper <zhangzeyu115@gmail.com>
2023-09-10 02:00:49 +0800

    Added add_user method

--- plugins/set/utils.py
+++ plugins/set/utils.py
@@ -1,4 +1,7 @@
 from nonebot import logger
+from os import getcwd
+from os.path import join
+import yaml
 
 def name(username, wxid):
     pass
--- plugins/utils.py
+++ plugins/utils.py
@@ -1,10 +1,31 @@
-from nonebot.adapters.onebot.v12 import MessageSegment,GroupMessageEvent
+from nonebot.adapters.onebot.v12 import MessageSegment, GroupMessageEvent
 import yaml
+from os import getcwd
+from os.path import join
+from nonebot.exception import IgnoredException
 
-def send(event:GroupMessageEvent, content:str, type:int=0):
+
+def send(event: GroupMessageEvent, content: str, type: int = 0):
     """
         type: 0是元气, 1是生气, 2是悲伤
     """
     title = "test title"
     emoticons = ['ᕕ( ᐛ )ᕗ', '(╯°□°）╯︵ ┻━┻', '(っ °Д °;)っ']
     return MessageSegment.mention(event.user_id) + MessageSegment.text(f"{title}，{content}{emoticons[type]}")
+
+
+def add_user(event: GroupMessageEvent):
+    path = join(getcwd(), r"\data\user_data.yaml")
+
+    with open(path, 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+
+    if event.get_user_id() in data.keys():
+        raise IgnoredException("User already exists!")
+    user_info = data["wxid_example"]
+    user_info["name"] = "*输入“/set -n 姓名”以录入姓名*"
+    user_info["pinyin"] = "weiluru"
+    user = {event.get_user_id(): user_info}
+
+    with open(path, 'a') as file:
+        yaml.dump(user, file)
