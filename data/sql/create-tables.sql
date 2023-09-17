1a6c062be28433fbc119d896770b39b00c86e045
Liusy12810 <liusy12810@outlook.com>
2023-09-17 11:52:54 +0800

    Cool

--- data/sql/create-tables.sql
+++ data/sql/create-tables.sql
@@ -6,7 +6,22 @@ create table
     `wechat_id` INTEGER not null,
     `username` varchar(255) not null,
     `pinyin` varchar(255) not null,
-    `admin` integer not null,
+    `admin` integer not null default 0,
+    `status` integer default 1
+  )
+
+create table
+  `log_user` (
+    `id` integer not null primary key autoincrement,
+    `created_at` timestamp not null default CURRENT_TIMESTAMP,
+    `user_id` integer not null,
+    `operation` varchar(255)
+  )
+
+create table
+  `title` (
+    `user_id` integer not null,
+    `title` varchar(10),
     `status` integer default 1
   )
 
@@ -21,17 +36,10 @@ create table
   )
 
 create table
-  `title` (
-    `user_id` integer not null,
-    `title` varchar(10),
-    `status` integer default 1
-  )
-
-create table
   `student` (
     `id` integer not null primary key autoincrement,
     `user_id` integer not null,
-    `created_at` timestamp not null default CURRENT_TIMESTAMP,
+    `year` integer not null,
     `grade` integer not null,
     `class` integer not null,
     `status` integer default 1
@@ -45,10 +53,21 @@ create table
   )
 
 create table
-  `lessons` (
+  `lesson` (
     `id` integer not null primary key autoincrement,
+    `year` integer not null,
     `grade` integer not null,
-    `lesson_type` varchar(255) not null,
-    `registered_lesson` varchar(255) not null,
+    `lesson_type` varchar(25) not null,
+    `lesson_name` varchar(25) not null,
+    `status` integer not null default 1
+  )
+
+create table
+  `lesson_table` (
+    `id` integer not null primary key autoincrement,
+    `year` integer not null,
+    `grade` integer not null,
+    `class` integer not null,
+    `table` varchar(255) not null,
     `status` integer not null default 1
   )
